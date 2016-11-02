c04d5ac5c1e8eac1b3b318dd0bde974ac043b272
Sara Sunshine <sarasunsh@gmail.com>
2016-11-01 20:29:17 -0400

    basic form works

--- browser/react/components/Experiment/ExperimentFormContainer.js
+++ browser/react/components/Experiment/ExperimentFormContainer.js
@@ -3,86 +3,16 @@
 import React from 'react';
 import { connect } from'react-redux';
 import ExperimentForm from './ExperimentForm';
+import ExperimentFormDecorator from './FormDecorator';
 import { addNewArm } from '../../ducks/experiment';
 
-// Here the HOC takes the 'dumb' form component and gives it a local state to track the title and then event handlers for when the experiment arm details are changed OR when the form is submitted. It also passes down the addNewArm action creator so the new arm can be sent to the store after submission
-function ExperimentFormDecorator(ExperimentFormComponent) {
-    return class StatefulExperimentForm extends React.Component {
-        constructor(props){
-            super(props);
-            this.state = {
-                description: '',
-                genotype: '',
-                treatment: '',
-                invalid: true
-            }
-            this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-            this.handleGenotypeChange = this.handleGenotypeChange.bind(this);
-            this.handleTreatmentChange = this.handleTreatmentChange.bind(this);
-            this.handleSubmitWithState = this.handleSubmitWithState.bind(this);
-        }
-
-        handleDescriptionChange(evt){
-            this.setState({
-                description: evt.target.value
-            });
-        }
-
-        handleGenotypeChange(evt){
-            this.setState({
-                genotype: evt.target.value,
-                invalid: evt.target.value.length > 16
-            });
-        }
-
-        handleTreatmentChange(evt){
-            this.setState({
-                treatment: evt.target.value
-            });
-        }
-
-        handleSubmitWithState(evt){
-            evt.preventDefault();
-            const newArm = {
-                title:this.state.description,
-                goal: 5,
-                genotype: this.state.genotype.toUpperCase(),
-                treatment: this.state.treatment
-            }
-            this.props.createArm(newArm);
-            this.setState({
-                description: '',
-                genotype: '',
-                treatment: '',
-                invalid: true
-            })
-        }
-
-        render(){
-            return (
-                <ExperimentFormComponent
-                    handleDescriptionChange={this.handleDescriptionChange}
-                    handleGenotypeChange={this.handleGenotypeChange}
-                    handleTreatmentChange={this.handleTreatmentChange}
-                    handleSubmit={this.handleSubmitWithState}
-                    description={this.state.description}
-                    genotype={this.state.genotype}
-                    treatment={this.state.treatment}
-                    goal={this.state.goal}
-                    invalid={this.state.invalid}
-                />
-            )
-        }
+const mapStateToProps = function(state){
+    console.log('mapstate', state.exptArms)
+    return {
+        exptArms: state.exptArms,
+        test: 'yay'
     }
 }
-
-// const mapStateToProps = function(state){
-//     console.log('mapstate', state.exptArms)
-//     return {
-//         exptArms: state.exptArms,
-//         test: 'do you see this'
-//     }
-// }
 
 const mapDispatchToProps = function (dispatch) {
   return {
@@ -94,5 +24,6 @@ const mapDispatchToProps = function (dispatch) {
 };
 
 const StatefulExperimentForm = ExperimentFormDecorator(ExperimentForm)
-const ExperimentFormContainer = connect(null, mapDispatchToProps)(StatefulExperimentForm);
+
+const ExperimentFormContainer = connect(mapStateToProps, mapDispatchToProps)(StatefulExperimentForm);
 export default ExperimentFormContainer;
--- server/app/routes/mice.js
+++ server/app/routes/mice.js
@@ -28,38 +28,3 @@ router.post('/', function (req, res, next) {
   .catch(next);
 });
 
-
-// router.param('albumId', function (req, res, next, id) {
-//   Album.scope('defaultScope', 'populated').findById(id)
-//   .then(function (album) {
-//     if (!album) {
-//       const err = Error('Album not found');
-//       err.status = 404;
-//       throw err
-//     }
-//     req.album = album;
-//     next();
-//     return null; // silences bluebird warning about promises inside of next
-//   })
-//   .catch(next);
-// });
-
-// router.get('/:albumId', function (req, res) {
-//   res.json(req.album);
-// });
-
-// router.get('/:albumId/image', function (req, res, next) {
-//   res.redirect(`/api/songs/${req.album.songs[0].id}/image`)
-// });
-
-// router.get('/:albumId/songs/', function (req, res) {
-//   res.json(req.album.songs);
-// });
-
-// router.get('/:albumId/songs/:songId', function (req, res) {
-//   const songToSend = req.album.songs.find(song => {
-//     return song.id === Number(req.params.songId);
-//   });
-//   if (!songToSend) return res.sendStatus(404);
-//   res.json(songToSend);
-// });
